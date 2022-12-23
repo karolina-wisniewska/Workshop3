54c9595739a9f2738675c1dad1d36bdd90e06e5c
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2022-12-23 22:12:02 +0100

    UserList: links

---
+++ Workshop3/src/main/java/pl/coderslab/users/UserEdit.java
@@ -0,0 +1,19 @@
+package pl.coderslab.users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/user/show")
+public class UserEdit extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            UserDAO userDao = new UserDAO();
+            request.setAttribute("users", userDao.findAll());
+
+        getServletContext().getRequestDispatcher("/users/add.jsp")
+                .forward(request, response);
+    }
+}
