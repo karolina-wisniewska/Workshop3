02fa06000f04ebb8df478da823e5bf4cb6620354
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2022-12-24 09:37:13 +0100

    UserEdit: update

--- Workshop3/src/main/java/pl/coderslab/users/UserEdit.java
+++ Workshop3/src/main/java/pl/coderslab/users/UserEdit.java
@@ -1,10 +1,13 @@
 package pl.coderslab.users;
 
+import com.mysql.cj.Session;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/user/edit")
@@ -13,7 +16,10 @@ public class UserEdit extends HttpServlet {
         String id = request.getParameter("id");
         UserDAO userDao = new UserDAO();
         User read = userDao.read(Integer.parseInt(id));
-        request.setAttribute("user", read);
+//        request.setAttribute("user", read);
+
+        HttpSession session = request.getSession();;
+        session.setAttribute("user",read);
 
         getServletContext().getRequestDispatcher("/users/edit.jsp")
                 .forward(request, response);
@@ -21,7 +27,17 @@ public class UserEdit extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession();
+
+        User userToEdit = (User) session.getAttribute("user");
+
+        userToEdit.setUserName(req.getParameter("name"));
+        userToEdit.setEmail(req.getParameter("email"));
+        userToEdit.setPassword(req.getParameter("password"));
+
         UserDAO userDao = new UserDAO();
-        req.setAttribute("users", userDao.findAll());
+        userDao.update(userToEdit);
+        resp.sendRedirect("/workshop/user/list");
     }
 }
