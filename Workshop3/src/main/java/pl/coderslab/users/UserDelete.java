f8df9d801ed52559c46541aa33616908562c9161
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2022-12-24 10:25:19 +0100

    UserEdit: delete

--- Workshop3/src/main/java/pl/coderslab/users/UserDelete.java
+++ Workshop3/src/main/java/pl/coderslab/users/UserDelete.java
@@ -5,15 +5,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/user/delete")
 public class UserDelete extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            UserDAO userDao = new UserDAO();
-            request.setAttribute("users", userDao.findAll());
+        String id = request.getParameter("id");
+        UserDAO userDao = new UserDAO();
+        userDao.delete(Integer.parseInt(id));
 
-        getServletContext().getRequestDispatcher("/users/add.jsp")
+        getServletContext().getRequestDispatcher("/users/list.jsp")
                 .forward(request, response);
     }
 }
